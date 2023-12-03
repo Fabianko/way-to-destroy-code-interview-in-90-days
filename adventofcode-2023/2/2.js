33305d3b2c3d14ccef534fef2fc016adcf23998a
Fabianko <fabian.arismendi@usach.cl>
2023-12-02 22:04:31 -0300

    ADD: part 2 of adventtocode-2023 day 2

--- adventofcode-2023/2/2.js
+++ adventofcode-2023/2/2.js
@@ -12,7 +12,7 @@ function main() {
     const lines = file.split(/\r?\n/);
     let resultado = 0;
     lines.forEach(line => {
-        resultado += validate(line);
+        resultado += validate2(line);
     });
     console.log(resultado);
     return resultado
@@ -20,7 +20,6 @@ function main() {
 
 // linea de ejemplo "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green" al sumar los valores de blue no pueden ser superiores a 12 segun config, lo mismo para green y red
 function validate(line){
-
     let id = parseInt(line.split(":")[0].split(" ")[1])
     let arr = line.split(":")[1].split(";")
     let flag = false
@@ -43,7 +42,45 @@ function validate(line){
         return 0
     }
     return id
-   
+}
+
+function validate2(line){
+    let id = parseInt(line.split(":")[0].split(" ")[1])
+    let arr = line.split(":")[1].split(";")
+    let flag = false
+    let temp = {
+        "blue": 0,
+        "green": 0,
+        "red": 0,
+    }
+    arr.forEach(element => {
+        let arr2 = element.split(",")
+        arr2.forEach(element2 => {
+            let arr3 = element2.split(" ")
+            if (arr3[2] == "blue") {
+                // si temp.blue es menor a arr3[1] entonces temp.blue = arr3[1]
+                if (parseInt(arr3[1]) > temp.blue){
+                    temp.blue = parseInt(arr3[1])
+                }
+               
+            }
+            else if (arr3[2] == "green") {
+                if (parseInt(arr3[1]) > temp.green){
+                    temp.green = parseInt(arr3[1])
+                }
+                
+            }
+            else if (arr3[2] == "red") {
+                if (parseInt(arr3[1]) > temp.red){
+                    temp.red = parseInt(arr3[1])
+                }
+                
+            }
+        });
+    });
+
+    let result = temp.blue * temp.green * temp.red
+    return result
 }
 
 main()  // 1
