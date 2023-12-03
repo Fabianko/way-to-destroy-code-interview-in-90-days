08d383f0d304aceea4075cfb220c45d5652907d3
Fabianko <fabian.arismendi@usach.cl>
2023-12-03 10:00:14 -0300

    ADD: opción optimizada según gepeto

--- adventjs/3.js
+++ adventjs/3.js
@@ -20,6 +20,15 @@ function findNaughtyStep(original, modified) {
     }
     return ''; // No hay cambios
   }
+
+  function findNaughtyStep(original, modified) {
+    for (let i = 0; i < Math.max(original.length, modified.length); i++) {
+      if (original[i] !== modified[i]) {
+        return original[i + 1] === modified[i] ? original[i] : modified[i];
+      }
+    }
+    return ''; 
+  }
 console.log(findNaughtyStep('abcd', 'abcde'))// 'e'
 
 
